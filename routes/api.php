50d6a57d470984dc015260447864069f22a03021
chrialge <chrialge@gmail.com>
2024-05-30 15:33:38 +0200

    add call api inside file api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Project;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('projects', function () {
+    $projects = Project::with('type', 'technologies')->get();
+    return response()->json([
+        'success' => true,
+        'projects' => $projects,
+    ]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
