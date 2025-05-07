5b01ac75d6c22134aaccf716a997a8ca1cd23d21
chrialge <chrialge@gmail.com>
2025-05-07 12:30:56 +0200

    add sidebar for device pc and tablet

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -39,93 +39,123 @@
 
                 <li>
                     <img class="logo" src="{{ asset('logo/logo_chrialge_bianco.png') }}" alt="">
-                    <button class="toggle_btn">
-                        <i class="ri-arrow-right-double-fill"></i>
+                    <button onclick="toggleSidebar()" id="toggle_btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                            fill="#e3e3e3">
+                            <path
+                                d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z" />
+                        </svg>
                     </button>
                 </li>
 
                 <li class="active">
                     <a href="{{ route('admin.dashboard') }}">
-                        <i class="fa-solid fa-table-columns"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                            fill="#e3e3e3">
+                            <path
+                                d="M520-600v-240h320v240H520ZM120-440v-400h320v400H120Zm400 320v-400h320v400H520Zm-400 0v-240h320v240H120Zm80-400h160v-240H200v240Zm400 320h160v-240H600v240Zm0-480h160v-80H600v80ZM200-200h160v-80H200v80Zm160-320Zm240-160Zm0 240ZM360-280Z" />
+                        </svg>
                         <span>Dashboard</span>
                     </a>
                 </li>
 
                 <li>
-                    <button class="dropdown_btn">
-                        <i class="fa-solid fa-folder-plus"></i>
+                    <button class="dropdown_btn" onclick="toggleSubMenu(this)">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                            fill="#e3e3e3">
+                            <path
+                                d="M560-320h80v-80h80v-80h-80v-80h-80v80h-80v80h80v80ZM160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Zm0-80h640v-400H447l-80-80H160v480Zm0 0v-480 480Z" />
+                        </svg>
                         <span>Crea</span>
-                        <i class="fa-solid fa-chevron-down"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                            fill="#e3e3e3">
+                            <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+                        </svg>
                     </button>
 
                     <ul class="sub-menu">
-                        <li>
-                            <a href="{{ route('admin.projects.create') }}">
-                                Progetto
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('admin.technologies.create') }}">
-                                Tecnlogia
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('admin.types.create') }}">
-                                Tipologia
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('admin.collaborators.create') }}">
-                                Collaboratore
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('admin.notes.create') }}">
-                                Nota
-                            </a>
-                        </li>
+                        <div>
+                            <li>
+                                <a href="{{ route('admin.projects.create') }}">
+                                    Progetto
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('admin.technologies.create') }}">
+                                    Tecnlogia
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('admin.types.create') }}">
+                                    Tipologia
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('admin.collaborators.create') }}">
+                                    Collaboratore
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('admin.notes.create') }}">
+                                    Nota
+                                </a>
+                            </li>
+                        </div>
                     </ul>
                 </li>
 
                 <li>
-                    <button class="dropdown_btn">
-                        <i class="ri-todo-fill"></i>
+                    <button class="dropdown_btn" onclick="toggleSubMenu(this)">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                            fill="#e3e3e3">
+                            <path
+                                d="M360-240h440v-107H360v107ZM160-613h120v-107H160v107Zm0 187h120v-107H160v107Zm0 186h120v-107H160v107Zm200-186h440v-107H360v107Zm0-187h440v-107H360v107ZM160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Z" />
+                        </svg>
                         <span>Tabelle</span>
-                        <i class="fa-solid fa-chevron-down"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                            fill="#e3e3e3">
+                            <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+                        </svg>
                     </button>
 
                     <ul class="sub-menu">
-                        <li>
-                            <a href="{{ route('admin.projects.index') }}">
-                                Progetto
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('admin.technologies.index') }}">
-                                Tecnlogia
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('admin.types.index') }}">
-                                Tipologia
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('admin.collaborators.index') }}">
-                                Collaboratore
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('admin.notes.index') }}">
-                                Nota
-                            </a>
-                        </li>
+                        <div>
+                            <li>
+                                <a href="{{ route('admin.projects.index') }}">
+                                    Progetto
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('admin.technologies.index') }}">
+                                    Tecnlogia
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('admin.types.index') }}">
+                                    Tipologia
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('admin.collaborators.index') }}">
+                                    Collaboratore
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('admin.notes.index') }}">
+                                    Nota
+                                </a>
+                            </li>
+                        </div>
                     </ul>
                 </li>
 
                 <li>
                     <a href="{{ route('profile.edit') }}">
-                        <i class="fa-solid fa-user-gear"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                            fill="#e3e3e3">
+                            <path
+                                d="M400-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM80-160v-112q0-33 17-62t47-44q51-26 115-44t141-18h14q6 0 12 2-8 18-13.5 37.5T404-360h-4q-71 0-127.5 18T180-306q-9 5-14.5 14t-5.5 20v32h252q6 21 16 41.5t22 38.5H80Zm560 40-12-60q-12-5-22.5-10.5T584-204l-58 18-40-68 46-40q-2-14-2-26t2-26l-46-40 40-68 58 18q11-8 21.5-13.5T628-460l12-60h80l12 60q12 5 22.5 11t21.5 15l58-20 40 70-46 40q2 12 2 25t-2 25l46 40-40 68-58-18q-11 8-21.5 13.5T732-180l-12 60h-80Zm40-120q33 0 56.5-23.5T760-320q0-33-23.5-56.5T680-400q-33 0-56.5 23.5T600-320q0 33 23.5 56.5T680-240ZM400-560q33 0 56.5-23.5T480-640q0-33-23.5-56.5T400-720q-33 0-56.5 23.5T320-640q0 33 23.5 56.5T400-560Zm0-80Zm12 400Z" />
+                        </svg>
                         <span>Profilo</span>
                     </a>
                 </li>
@@ -135,7 +165,11 @@
                     <a class="sidebar__link" href="{{ route('logout') }}"
                         onclick="event.preventDefault();
                                  document.getElementById('logout-form').submit();">
-                        <i class="ri-logout-box-r-fill"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                            fill="#e3e3e3">
+                            <path
+                                d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
+                        </svg>
                         <span>Log Out</span>
                     </a>
 
@@ -153,6 +187,8 @@
             @yield('content')
         </main>
     </div>
+
+    <script src="{{ asset('js/sidebar.js') }}"></script>
 </body>
 
 </html>
